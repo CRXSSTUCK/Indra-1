df805b9468f06339d4437bb08f785d5030ebe57e
sfleming <sfleming@flemingresearch.com>
2014-04-17 12:08:47 -0500

    starting account parsing build

---
+++ pkg/account/account.py
@@ -0,0 +1,10 @@
+class Account():
+	def __init__(self):
+		self.uname = None
+		self.password = None
+		
+		self.rewardPoints = 0
+		self.lifetimeEarnings = 0
+	def __str__(self):
+		return "this is an account"
+		
---
+++ pkg/account/account_manager.py
@@ -0,0 +1,11 @@
+class AccountManager():
+	def __init__(self):
+		self.accounts = list()
+	
+	def __init__(self, accountsJsonPath)
+		if accountsJsonPath is None:
+			except Exception:
+				raise Exception("path to accounts.json is None")
+		
+		self.accounts = __parse_accounts_json(accountsJsonPath)
+		
